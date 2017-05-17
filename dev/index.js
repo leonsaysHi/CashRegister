cb6502056996903e0c4df4e47df459876de35b19
leonsaysHi <leon.ronssin@gmail.com>
2017-05-17 16:59:58 +0200

    add foundation css

--- dev/index.js
+++ dev/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CashRegister from './components/CashRegister.jsx';
 
+require('./scss/main.scss');
+
 ReactDOM.render(<CashRegister />, document.getElementById('root'));
